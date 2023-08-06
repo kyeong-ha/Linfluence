6e2b601517b0237bc4b4ed33fea4795a431a4ee9
KyeongHa <rudgk030@hanyang.ac.kr>
2023-08-06 19:09:35 +0900

    2023-08-06

--- private_app/backend/server.js
+++ private_app/backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+// const multer = require('multer');
 
 // variable
 const PORT = 5001;
--- private_app/frontend/src/App.js
+++ private_app/frontend/src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
+import Axios from "axios";
 
 function App() {
+  useEffect(() => {
+    Axios.get("http://192.168.219.105:5001/api").then((res) =>
+      console.log(res.data.title)
+    );
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{process.env.REACT_APP_PORT}</h1>
       </header>
     </div>
   );
