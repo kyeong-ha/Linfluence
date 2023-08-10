a00ca25206b35b4abf1a946770abde4ca15034a5
KyeongHa <rudgk030@hanyang.ac.kr>
2023-08-10 23:53:30 +0900

    2023-08-10

--- public_server/src/app.tsx
+++ public_server/src/app.tsx
@@ -1,6 +1,8 @@
 import express, { Request, Response } from "express";
+import * as dotenv from 'dotenv';
 import path from "path";
 
+dotenv.config();
 const app = express();
 
 app.use(express.static(path.join(__dirname, '../public')));
@@ -8,6 +10,6 @@ app.get('/', (req: Request, res: Response) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-app.listen('8001', () => {
+app.listen(process.env.PORT, () => {
     console.log('Server started');
 });
