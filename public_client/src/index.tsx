e02004c02032dab6eaabae24744c2be4c7c1d906
KyeongHa <rudgk030@hanyang.ac.kr>
2023-08-12 12:25:39 +0900

    2023-08-12

---
+++ public_client/src/index.tsx
@@ -0,0 +1,16 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./App";
+
+// const root = ReactDOM.createRoot(
+//     document.getElementById("root") as HTMLElement
+// );
+// root.render(
+//     <React.StrictMode>
+//         <App />
+//     </React.StrictMode>
+// );
+
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<div>Hello World</div>, document.getElementById('app'));
