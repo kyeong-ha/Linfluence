8dbae2d71121a89f3becd07c82ea3d96f19f2ff5
KyeongHa <rudgk030@hanyang.ac.kr>
2023-08-16 00:05:37 +0900

    2023-08-16

--- public_client/src/components/headers/ProfileHeader.tsx
+++ public_client/src/components/headers/ProfileHeader.tsx
@@ -1,15 +1,25 @@
-import '../../styles/influencer.scss';
+import '../../styles/influencer/influencer.scss';
 
 export default function ProfileHeader() {
     return (
         <header className='profile-header'>
             <div className='back-icon'>뒤로가기</div>
-
-            <div className='profile-contents'>
-                <div className='profile-img'></div>
-                <div>인플루언서 이름</div>
-                <div>소개글</div>
-                <div className='profile-nav wrapper'>하단 nav</div>
+            <div className='profile-img-wrap'>
+                <div className='profile-img'>  
+                </div>
+            </div>
+            <div className='profile-contents-wrap'>
+                <div className='profile-contents'>
+                    <h2>인플루언서 이름</h2>
+                    <p>소개글</p>
+                    <div className='profile-nav wrapper'>
+                        <div className='nav-button'>
+                            <span>게시물</span>
+                            <span>스토어</span>
+                        </div>
+                        <div>필터</div>
+                    </div>
+                </div>
             </div>
         </header>
     )
