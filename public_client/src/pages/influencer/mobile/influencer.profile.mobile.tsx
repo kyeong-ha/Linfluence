e94c04daaca96c807f6191b5dd85c5d1e73aa158
KyeongHa <rudgk030@hanyang.ac.kr>
2023-08-16 00:05:27 +0900

    2023-08-16

--- public_client/src/pages/influencer/mobile/influencer.profile.mobile.tsx
+++ public_client/src/pages/influencer/mobile/influencer.profile.mobile.tsx
@@ -1,4 +1,6 @@
 import ProfileHeader from '../../../components/headers/ProfileHeader';
+import '../../../styles/influencer/influencer.container.scss';
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 
@@ -8,7 +10,10 @@ export function ProfileMobile(){
             <ProfileHeader />
             <div className='container'>
                 <div className='post-list'>
-                    <div className='post'>동영상 container</div>
+                    <div className='post'>
+                        동영상 container
+                        <div className='post-title'>title</div>
+                    </div>
                     <div className='post'>동영상 container</div>
                     <div className='post'>동영상 container</div>
                     <div className='post'>동영상 container</div>
